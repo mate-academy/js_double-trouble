25d562782538d89ac454c013535013b6fb05956d
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-09 21:35:47 +0300

    Yevhen Kryvdiuk

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -26,7 +26,15 @@
  * @return {number[]} - array of remained numbers
  */
 function doubleTrouble(array, target) {
-  // write code here
+  const filteredArray = array.slice();
+  for (let i = 0; i < filteredArray.length; i++) {
+    let sum = filteredArray[i] + filteredArray[i + 1];
+    if (sum === target) {
+      filteredArray.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return filteredArray;
 }
 
 module.exports = doubleTrouble;
