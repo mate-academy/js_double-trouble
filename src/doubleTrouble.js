3f4fd64d56d3d530bd061f5be5e9ddd0fea00f31
unknown <dumavb777@gmail.com>
2019-04-10 13:15:26 +0300

    'Solution'

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -28,7 +28,7 @@
 function doubleTrouble(array, target) {
   for (let i = 0; i <= array.length; i++) {
     if (array[i] + array[ i + 1 ] === target) {
-      array.splice(i, 1);
+      array.splice(i + 1, 1);
       i--;
     }
   }
