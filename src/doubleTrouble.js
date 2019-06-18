532c6bbf85e37b580f01f0afdcacb7c47dd8336c
Serhii <davydko.serhiy@gmail.com>
2019-06-18 17:51:45 +0300

    create new object

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -24,13 +24,14 @@
  * @return {number[]} - array of remained numbers
  */
 function doubleTrouble(array, target) {
-  for (let i = 0; i < array.length - 1; i++) {
-    if ((array[i] + array[i + 1]) === target) {
-      array.splice(i + 1, 1);
+  const newArr = JSON.parse(JSON.stringify(array));
+  for (let i = 0; i < newArr.length - 1; i++) {
+    if ((newArr[i] + newArr[i + 1]) === target) {
+      newArr.splice(i + 1, 1);
       i--;
     }
   }
-  return array;
+  return newArr;
   // write code here
 }
 
