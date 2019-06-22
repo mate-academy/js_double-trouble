f50c81b9ca1b28de4bb8597eb299d7ab23711708
alexandr <alexandrov6927@gmail.com>
2019-06-22 19:59:38 +0300

    'solution'

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -3,28 +3,38 @@
 /**
  * Implement doubleTrouble function:
  *
- * Function takes an array of integers (array), and a target (target), you must
- * find out if any two consecutive numbers in the array sum to target. If so,
- * remove the second number. Work through the array from left to right.
+ * Function takes an newArray of integers (newArray), and a target (target), you
+ * must
+ * find out if any two consecutive numbers in the newArray sum to target. If so,
+ * remove the second number. Work through the newArray from left to right.
  *
  * Example:
  *
- * const array = [1, 2, 3, 4, 5]
+ * const newArray = [1, 2, 3, 4, 5]
  * const target = 3
  *
- * 1+2 = target, so remove 2. No other pairs = target, so rest of array remains:
+ * 1+2 = target, so remove 2. No other pairs = target, so rest of newArray
+ * remains:
  *
  * [1, 3, 4, 5]
  *
- * Return the resulting array.
+ * Return the resulting newArray.
  *
  * @param {number[]} array
  * @param {number} target
  *
- * @return {number[]} - array of remained numbers
+ * @return {number[]} - newArray of remained numbers
  */
 function doubleTrouble(array, target) {
-  // write code here
+  const newArray = array;
+
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] + newArray[i + 1] === target) {
+      newArray.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return newArray;
 }
 
 module.exports = doubleTrouble;
