28acff2a58954470313d38b6a9f996d1bdf62e24
Andrii <farikpite@gmail.com>
2019-10-02 13:11:04 +0300

    Solution

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -25,9 +25,9 @@
  */
 function doubleTrouble(numbers, target) {
   const arr = numbers.slice();
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] + arr[i + 1] === target) {
-      arr.splice(i + 1, 1);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] + arr[i - 1] === target) {
+      arr.splice(i, 1);
       i--;
     }
   }
