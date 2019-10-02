e74a9081635e015edff3aed44cf60d674cd36fb6
Sijey <popov040391@gmail.com>
2019-10-02 09:23:49 +0300

    Solution

--- src/doubleTrouble.js
+++ src/doubleTrouble.js
@@ -24,7 +24,16 @@
  * @return {number[]} - array of remained numbers
  */
 function doubleTrouble(numbers, target) {
-  // write code here
+  const allNumbers = [...numbers];
+
+  for (let i = 0; i < allNumbers.length; i++) {
+    if (allNumbers[i] + allNumbers[i + 1] === target) {
+      allNumbers.splice(i + 1, 1);
+      i--;
+    }
+  }
+
+  return allNumbers;
 }
 
 module.exports = doubleTrouble;
